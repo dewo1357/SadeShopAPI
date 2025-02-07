95574ff656b1ce3167757812d47cbd195323011b
dewo1357 <dewo135789@gmail.com>
2025-02-07 23:40:04 +0700

    ten

--- hapi.js
+++ hapi.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-undef */
+// eslint-disable no-undef */
 
 const serverless = require('serverless-http');
-const init = require('index.js')
+const init = require('./index.js');
 
 let handler;
 
 module.exports.handler = async (req, res) => {
     if (!handler) {
-      const server = await init();
-      handler = serverless(server.listener);
+        const server = await init();
+        handler = serverless(server.listener);
     }
     return handler(req, res);
-  };
+};
