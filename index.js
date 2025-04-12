bc961366ac31f43b0e892cd776e83477a117f081
dewo1357 <dewo135789@gmail.com>
2025-04-12 11:44:49 +0700

    Add Project

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const init = async () => {
 
     const io = new Server(server.listener, {
         cors: {
-            origin: "https://sade-shop-fe.vercel.app" //"http://localhost:5173"
+            origin: ["https://sade-shop-fe.vercel.app","http://localhost:5173"]
         }
     });
 
