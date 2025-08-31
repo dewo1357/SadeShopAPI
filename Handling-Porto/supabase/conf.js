a512856e647883f7be8e03eb47fe86ab47dece10
dewo1357 <dewo135789@gmail.com>
2025-08-31 23:59:37 +0700

    fix project prtofolio

--- Handling-Porto/supabase/conf.js
+++ Handling-Porto/supabase/conf.js
@@ -4,6 +4,8 @@ require('dotenv').config({path : './.env'})
 const url = process.env.url
 const anon_key = process.env.anon_key_porto
 
+console.log("url", url)
+console.log("Anon key : ", anon_key)
 // Create a single supabase client for interacting with your database
 const supabase = createClient(url, anon_key)
 
